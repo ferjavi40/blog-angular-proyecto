5dccc2d25c5810e1c61cfb7fbca4fac10b550617
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-15 15:41:50 +0100

    antes de hacer home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ContactoComponent } from './blog/pages/contacto/contacto.component';
 import { BlogesComponent } from './blog/pages/bloges/bloges.component';
 import { BlogComponent } from './blog/components/blog/blog.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './blog/pages/home/home.component';
 
 
 @NgModule({
@@ -13,11 +16,14 @@ import { BlogComponent } from './blog/components/blog/blog.component';
     AppComponent,
     ContactoComponent,
     BlogesComponent,
-    BlogComponent
+    BlogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/blog/components/blog/blog.component.ts
+++ src/app/blog/components/blog/blog.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent  {
 
-  constructor() { }
+  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  ngOnInit(): void {
+  paused = false;
+  unpauseOnArrow = true;
+  pauseOnIndicator = true;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
   }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
+
 
 }
